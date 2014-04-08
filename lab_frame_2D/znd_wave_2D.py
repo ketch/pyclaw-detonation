886b82bb9c9eeb9e008ff7879b5783cbc4d7add3
Luiz Faria <maltezfaria@gmail.com>
2014-04-08 15:39:38 +0300

    Set up the initial conditions correctly in parallel

--- lab_frame_2D/znd_wave_2D.py
+++ lab_frame_2D/znd_wave_2D.py
@@ -2,7 +2,9 @@
 # encoding: utf-8
 
 import numpy as np
+import mpi4py.MPI as mpi
 from setplot import setplot
+#from pprint import pprint
 
 gamma = 1.2
 gamma1 = gamma - 1.
@@ -14,50 +16,73 @@ Ea = theta/(gamma1**2)
 #qheat = 50
 #Ea = 20
 
-print('Heat release Q={} and activation energy={}'.format(qheat,Ea))
+if mpi.COMM_WORLD.Get_rank()==0:
+    print('Heat release Q = {} and activation energy = {}'.format(qheat,Ea)) 
 
 T_ign = 1.005
 
 xmax = 30.
-ymax = 40.
+ymax = 50.
+mx = 200
+my = 400
 xs = 25.
+tfinal = 1000.
+num_output_times=500
 
 def b4step(solver, state):
     pass
 
-def qinit(state,xs=xs):
+def qinit(state,domain,xs=xs):
     import steadyState as steadyState
 
     grid = state.grid
-    
-    # Create an array with fortran native ordering
+    ((ixlower,ixupper),(iylower,iyupper)) = domain.patch._da.getRanges()
+    #print(ixlower, ixupper)
+
+    dx = grid.delta[0] #Could be problem for non-uniform grid
+    xglobal = np.linspace(dx/2,xmax-dx/2,mx)
+
     x =grid.x.centers
     y =grid.y.centers
-    xl = x[x<xs]
+    xl = xglobal[xglobal<xs]
 
     rhol, Ul, pl, laml, D, k = steadyState.steadyState(qheat, Ea, gamma, xl)
     ul = Ul + D
     Yl = 1- laml
 
-    rhor = 1.*np.ones(np.shape(x[x>=xs]))
-    ur = 0.0*np.ones(np.shape(x[x>=xs]))
-    pr = 1*np.ones(np.shape(x[x>=xs]))
-    Yr = 1.*np.ones(np.shape(x[x>=xs]))
+    rhor = 1.*np.ones(np.shape(xglobal[xglobal>=xs]))
+    ur = 0.0*np.ones(np.shape(xglobal[xglobal>=xs]))
+    pr = 1*np.ones(np.shape(xglobal[xglobal>=xs]))
+    Yr = 1.*np.ones(np.shape(xglobal[xglobal>=xs]))
 
-    rho = np.append(rhol, rhor)
-    u = np.append(ul, ur)
-    v = 0.*u
-    p = np.append(pl, pr)
-    Y = np.append(Yl, Yr)
+    rhog = np.append(rhol, rhor)
+    ug = np.append(ul, ur)
+    vg = 0.*ug
+    pg = np.append(pl, pr)
+    Yg = np.append(Yl, Yr)
+
+
+
+    rho = rhog[ixlower:ixupper]
+
+    u = ug[ixlower:ixupper]
+    v = vg[ixlower:ixupper]
+    p = pg[ixlower:ixupper]
+    Y = Yg[ixlower:ixupper]
 
     for i in range(y.size):
-        pert = 0.00001 * np.sin(2*np.pi*y[i]/ymax) * (x<xs)
+        #pprint(y.size)
+        pert = 0.01 * np.sin(2*8*np.pi*y[i]/ymax) * (x<xs)
+        #pert=0
+        #print(np.shape(rho),np.shape(x))
         state.q[0,:,i] = rho + pert
-        state.q[1,:,i] = rho * u + pert
+        state.q[1,:,i] = rho * u +pert
         state.q[2,:,i] = 0. + pert
-        state.q[3,:,i] = p/gamma1 + rho*(u**2 + v**2)/2 + qheat * rho * Y + pert
-        state.q[4,:,i] = rho * Y 
+        state.q[3,:,i] = p/gamma1 + rho*(u**2 + v**2)/2 + qheat * rho * Y  + pert
+        state.q[4,:,i] = rho * Y
         
+    
+
     state.problem_data['fspeed']= D
     state.problem_data['k']= k
     
@@ -84,8 +109,8 @@ def omega(state):
     
 
 def setup(use_petsc=False,kernel_language='Fortran',solver_type='classic',
-          outdir='_output', disable_output=False, mx=100, my=100, tfinal=5,
-          num_output_times = 5):
+          outdir='_output', disable_output=False, mx=mx, my=my, tfinal=tfinal,
+          num_output_times = num_output_times):
     """
     Solve the reactive Euler equations of compressible fluid dynamics.
     """
@@ -115,7 +140,7 @@ def setup(use_petsc=False,kernel_language='Fortran',solver_type='classic',
     state.problem_data['qheat']= qheat
 
     # The k and fspeed problem data are set inside qinit
-    qinit(state)
+    qinit(state,domain)
 
     solver.cfl_max = 0.5
     solver.cfl_desired = 0.45
@@ -123,8 +148,8 @@ def setup(use_petsc=False,kernel_language='Fortran',solver_type='classic',
     solver.source_split = 1
     solver.bc_lower[0]=pyclaw.BC.extrap
     solver.bc_upper[0]=pyclaw.BC.extrap
-    solver.bc_lower[1]=pyclaw.BC.wall
-    solver.bc_upper[1]=pyclaw.BC.wall
+    solver.bc_lower[1]=pyclaw.BC.periodic
+    solver.bc_upper[1]=pyclaw.BC.periodic
 
     claw = pyclaw.Controller()
     claw.solution = pyclaw.Solution(state,domain)
